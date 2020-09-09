998d99792d1fa37cdd4bfa353bd1d9d4e9af00e5
jiyoonhur <jiyoon1156@gmail.com>
2020-09-09 16:15:09 +0900

    ADD: addrlen

--- src/Server.hpp
+++ src/Server.hpp
@@ -57,12 +57,12 @@ class Server
 
 			void init_server(void)
 			{
-				if ((sockfd_ = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
-				{ 
+				if ((sockfd_ = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+				{
 					std::cerr << "Error: " << name_ << " init_server() socket: " << strerror(errno) << std::endl;
-					exit(1); 
-				} 
-				
+					exit(1);
+				}
+
 				int opt = 1;
 				// Forcefully attaching socket to the port 8080
 					if( setsockopt(sockfd_, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0 )
@@ -70,23 +70,23 @@ class Server
 					perror("setsockopt");
 					exit(EXIT_FAILURE);
 				}
-			
+
 				ft_memset((void *)&server_addr_, 0, (unsigned long)sizeof(server_addr_)); // 왜 libft ft_memset link가 안 될까?
-				server_addr_.sin_family = AF_INET; 
-				server_addr_.sin_addr.s_addr = INADDR_ANY; 
-				server_addr_.sin_port = htons( this->port_ ); 
-				
-				// Forcefully attaching socket to the port 8080 
-				if (bind(sockfd_, (struct sockaddr *)&server_addr_, sizeof(server_addr_)) == -1) 
-				{ 
+				server_addr_.sin_family = AF_INET;
+				server_addr_.sin_addr.s_addr = INADDR_ANY;
+				server_addr_.sin_port = htons( this->port_ );
+
+				// Forcefully attaching socket to the port 8080
+				if (bind(sockfd_, (struct sockaddr *)&server_addr_, sizeof(server_addr_)) == -1)
+				{
 					std::cerr << "Error: " << name_ << " init_server() bind: " << strerror(errno) << std::endl;
 					exit(1);
 				}
 
-				if (listen(sockfd_, 10) == -1) 
-				{ 
+				if (listen(sockfd_, 10) == -1)
+				{
 					std::cerr << "Error: " << name_ << " init_server() listen: " << strerror(errno) << std::endl;
-					exit(1); 
+					exit(1);
 				}
 				if (fcntl(sockfd_, F_SETFL, O_NONBLOCK) == -1)
 				{
@@ -94,79 +94,79 @@ class Server
 					exit(1);
 				}
 
-		
+
 
 				char buf[3001];
 				int new_socket;
 
 				fd_set readfds;
 				int	max_sd, client_socket[30], max_clients = 30, sd, activity;
-				
-				
+
+				socklen_t addrlen = sizeof(server_addr_);
 
 				while (1)
 				{
 					FD_ZERO(&readfds);
 					FD_SET(sockfd_, &readfds);
 					max_sd = sockfd_;
-					for (int i = 0 ; i < max_clients; i++)   
-					{   
-						//socket descriptor  
-						sd = client_socket[i];   
-								
-						//if valid socket descriptor then add to read list  
-						if(sd > 0)   
-							FD_SET( sd , &readfds);   
-								
-						//highest file descriptor number, need it for the select function  
-						if(sd > max_sd)   
-							max_sd = sd;   
+					for (int i = 0 ; i < max_clients; i++)
+					{
+						//socket descriptor
+						sd = client_socket[i];
+
+						//if valid socket descriptor then add to read list
+						if(sd > 0)
+							FD_SET( sd , &readfds);
+
+						//highest file descriptor number, need it for the select function
+						if(sd > max_sd)
+							max_sd = sd;
 					}
 
-					activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);   
-						
-					if ((activity < 0) && (errno!=EINTR))   
+					activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);
+
+					if ((activity < 0) && (errno!=EINTR))
 					{
-						printf("select error");   
+						printf("select error");
 					}
 
-					if (FD_ISSET(sockfd_, &readfds))   
+					if (FD_ISSET(sockfd_, &readfds))
 					{
-						if ((new_socket = accept(sockfd_, (struct sockaddr *)&server_addr_, &addrlen)) == -1) 
-						{ 
+						if ((new_socket = accept(sockfd_, (struct sockaddr *)&server_addr_, &addrlen)) == -1)
+						{
 							std::cerr << "Error: " << name_ << " init_server() accept: " << strerror(errno) << std::endl;
 							exit(1);
 						}
 						ft_memset(buf, 0, 3001);
-						for (int i = 0; i < max_clients; i++)   
+						for (int i = 0; i < max_clients; i++)
 						{
-							//if position is empty  
-							if(client_socket[i] == 0)   
-							{   
-								client_socket[i] = new_socket;   
+							//if position is empty
+							if(client_socket[i] == 0)
+							{
+								client_socket[i] = new_socket;
 								printf("Adding to list of sockets as %d\n" , i);
-								break;   
+								break;
 							}
 						}
-					}	
+					}
 
-					for (int i = 0; i < max_clients; i++)   
+					for (int i = 0; i < max_clients; i++)
 					{
-						sd = client_socket[i];  
-							
-						if (FD_ISSET(sd , &readfds))   
-						{   
-							//Check if it was for closing , and also read the  
-							//incoming message  
+						sd = client_socket[i];
+
+						if (FD_ISSET(sd , &readfds))
+						{
+							//Check if it was for closing , and also read the
+							//incoming message
 							int	valread;
-							if ((valread = read( sd , buf, 1024)) == 0)   
+							if ((valread = read( sd , buf, 1024)) == 0)
 							{
 								close( sd );
 								client_socket[i] = 0;
 							}
-								
-							//Echo back the message that came in  
-							else 
+
+							//Echo back the message that came in
+							else
 							{
 								if (fcntl(sd, F_SETFL, O_NONBLOCK) == -1)
 								{
@@ -174,30 +174,30 @@ class Server
 									exit(1);
 								}
 								//while (valread = read)
-								//set the string terminating NULL byte on the end  
-								//of the data read 
+								//set the string terminating NULL byte on the end
+								//of the data read
 								int n = valread;
 								while ((valread = read(sd , buf + n, 1024)) > 0 && n < 1024)
 								{
 									n += valread;
 									std::cout << "n is " << n << std::endl;
 								}
-								
+
 								Message m;
-								
+
 								m.receiveRequest(buf);
 								m.sendRespond(sd);
 								std::cout << "Server sent message" << std::endl;
 
-								//buffer[valread] = '\0';   
-								//send(sd , buffer , strlen(buffer) , 0 );   
-							} 
-							ft_memset(buf, 0, 3001); 
-						}   
-					}  
+								//buffer[valread] = '\0';
+								//send(sd , buffer , strlen(buffer) , 0 );
+							}
+							ft_memset(buf, 0, 3001);
+						}
+					}
 
 
-				}	
+				}
 			}
 
 };
